2ae5392d835ff320e09e6b7da91bff0253be90e8
Kenzananas <bouzidkenza1@gmail.com>
2021-04-29 17:54:55 +0200

    visualize attention

---
+++ models/vit-keras/visualize.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+import layers, vit
+
+
+
+def attention_map(model, image):
+    """Get an attention map for an image and model using the technique
+    described in Appendix D.7 in the paper (unofficial).
+
+    Args:
+        model: A ViT model
+        image: An image for which we will compute the attention map.
+    """
+    size = model.input_shape[1]
+    grid_size = int(np.sqrt(model.layers[5].output_shape[0][-2]))
+
+    # Prepare the input
+    X = vit.preprocess_inputs(cv2.resize(image, (size, size)))[
+        np.newaxis, :]  # type: ignore
+
+    # Get the attention weights from each transformer.
+    outputs = [
+        l.output[1] for l in model.layers if isinstance(l, layers.TransformerBlock)
+    ]
+    weights = np.array(
+        tf.keras.models.Model(inputs=model.inputs, outputs=outputs).predict(X)
+    )
+    num_layers = weights.shape[0]
+    num_heads = weights.shape[2]
+    reshaped = weights.reshape(
+        (num_layers, num_heads, grid_size ** 2 , grid_size ** 2)
+    )
+
+    # From Appendix D.6 in the paper ...
+    # Average the attention weights across all heads.
+    reshaped = reshaped.mean(axis=1)
+
+    # From Section 3 in https://arxiv.org/pdf/2005.00928.pdf ...
+    # To account for residual connections, we add an identity matrix to the
+    # attention matrix and re-normalize the weights.
+    reshaped = reshaped + np.eye(reshaped.shape[1])
+    reshaped = reshaped / reshaped.sum(axis=(1, 2))[:, np.newaxis, np.newaxis]
+
+    # Recursively multiply the weight matrices
+    v = reshaped[-1]
+    for n in range(1, len(reshaped)):
+        v = np.matmul(v, reshaped[-1 - n])
+
+    # Attention from the output token to the input space.
+    mask = v[0, :].reshape(grid_size, grid_size)
+    mask = cv2.resize(mask / mask.max(), (image.shape[1], image.shape[0]))[
+        ..., np.newaxis
+    ]
+    return (mask * image).astype("uint8")
