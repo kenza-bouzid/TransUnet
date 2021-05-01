ad72edb2e1802d732c89a9cf36b35734556fc688
Kenzananas <bouzidkenza1@gmail.com>
2021-05-01 18:05:50 +0200

    clean tranunet file

--- models/transunet.py
+++ models/transunet.py
@@ -36,10 +36,10 @@ class TransUnet():
         self.kernel_size = config.kernel_size
         self.upsampling_factor = config.upsampling_factor
         self.vit = config.vit
-        self.model = self.build_transunet()
+        self.model = self.build_model()
 
 
-    def build_transunet(self):
+    def build_model(self):
         # Tranformer Encoder
         assert self.image_size % self.patch_size == 0, "image_size must be a multiple of patch_size"
         x = tf.keras.layers.Input(shape=(self.image_size, self.image_size, 3))
@@ -64,6 +64,7 @@ class TransUnet():
         y = tf.keras.layers.LayerNormalization(
             epsilon=1e-6, name="Transformer/encoder_norm"
         )(y)
+
         ## Segmentation Head
         n_patch_sqrt = (self.image_size//self.patch_size)
 
@@ -130,144 +131,7 @@ class TransUnet():
         dices = tf.where(tf.math.is_finite(dices), dices, tf.zeros_like(dices))
         return tf.reduce_mean(dices)
 
-    
 
-
-    
-
-
-def build_model(
-    image_size: int,
-    patch_size: int,
-    num_layers: int,
-    hidden_size: int,
-    num_heads: int,
-    name: str,
-    mlp_dim: int,
-    dropout=0.1
-):
-    """Build a ViT model.
-
-    Args:
-        image_size: The size of input images.
-        patch_size: The size of each patch (must fit evenly in image_size)
-        num_layers: The number of transformer layers to use.
-        hidden_size: The number of filters to use
-        num_heads: The number of transformer heads
-        mlp_dim: The number of dimensions for the MLP output in the transformers.
-        dropout_rate: fraction of the units to drop for dense layers.
-    """
-    assert image_size % patch_size == 0, "image_size must be a multiple of patch_size"
-    x = tf.keras.layers.Input(shape=(image_size, image_size, 3))
-    y = tf.keras.layers.Conv2D(
-        filters=hidden_size,
-        kernel_size=patch_size,
-        strides=patch_size,
-        padding="valid",
-        name="embedding",
-        trainable=False
-    )(x)
-    y = tf.keras.layers.Reshape((y.shape[1] * y.shape[2], hidden_size))(y)
-    y = layers.AddPositionEmbs(name="Transformer/posembed_input")(y)
-    for n in range(num_layers):
-        y, _ = layers.TransformerBlock(
-            n_heads=num_heads,
-            mlp_dim=mlp_dim,
-            dropout=dropout,
-            name=f"Transformer/encoderblock_{n}",
-        )(y)
-    y = tf.keras.layers.LayerNormalization(
-        epsilon=1e-6, name="Transformer/encoder_norm"
-    )(y)
-    n_patch_sqrt = (image_size//patch_size)
-    y = tf.keras.layers.Reshape(target_shape=[n_patch_sqrt, n_patch_sqrt, hidden_size])(y)
-    y = layers.SegmentationHead(**CONFIG_SEG_HEAD)(y)
-    return tf.keras.models.Model(inputs=x, outputs=y, name=name)
-
-
-
-def vit_b16(
-    image_size: int = 224,
-    pretrained=True,
-    weights="imagenet21k+imagenet2012",
-):
-    """Build ViT-B16. All arguments passed to build_model."""
-    model = build_model(
-        **CONFIG_B,
-        name="vit-b16",
-        patch_size=16,
-        image_size=image_size
-    )
-    if pretrained:
-        load_pretrained(
-            size="B_16", weights=weights, model=model
-        )
-    return model
-
-
-def vit_b32(
-    image_size: int = 224,
-    pretrained=True,
-    weights="imagenet21k+imagenet2012",
-):
-    """Build ViT-B32. All arguments passed to build_model."""
-    model = build_model(
-        **CONFIG_B,
-        name="vit-b32",
-        patch_size=32,
-        image_size=image_size,
-    )
-    if pretrained:
-        load_pretrained(
-            size="B_32", weights=weights, model=model
-        )
-    return model
-
-
-def vit_l16(
-    image_size: int = 384,
-    pretrained=True,
-    weights="imagenet21k+imagenet2012",
-):
-    """Build ViT-L16. All arguments passed to build_model."""
-    model = build_model(
-        **CONFIG_L,
-        patch_size=16,
-        name="vit-l16",
-        image_size=image_size,
-    )
-    if pretrained:
-        load_pretrained(
-            size="L_16", weights=weights, model=model
-        )
-    return model
-
-
-def vit_l32(
-    image_size: int = 384,
-    pretrained=True,
-    weights="imagenet21k+imagenet2012",
-):
-    """Build ViT-L32. All arguments passed to build_model."""
-    model = build_model(
-        **CONFIG_L,
-        patch_size=32,
-        name="vit-l32",
-        image_size=image_size,
-    )
-    if pretrained:
-        load_pretrained(
-            size="L_32", weights=weights, model=model
-        )
-    return model
-
-
-CONFIG_SEG_HEAD = {
-    "name": "None",
-    "filters": 9,
-    "kernel_size": 1,
-    "upsampling_factor": 16
-}
 
 
 
