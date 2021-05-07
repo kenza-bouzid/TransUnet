8998ed235326bc2dc8613564e6d6474d9d62a19b
miszkur <miszkurka.aga@gmail.com>
2021-05-07 17:34:08 +0200

    reuse previous config

--- experiments/config.py
+++ experiments/config.py
@@ -35,22 +35,11 @@ def get_b16_none():
 
 def get_b16_cup():
     """Returns the ViT-B/16 configuration."""
-    config = ml_collections.ConfigDict()
-    config.vit = "B_16"
-    config.image_size = 512
-    config.patch_size = 16
-    config.n_layers = 12
-    config.hidden_size = 768
-    config.n_heads = 12
+    config = get_b16_none()
     config.name = "b16_cup"
-    config.mlp_dim = 3072
-    config.dropout = 0.1
-    config.filters = 9
-    config.kernel_size = 1
     config.upsampling_factor = 1
     config.decoder_channels = [256, 128, 64, 16]
     config.n_skip = 0
-    config.hybrid = False
     return config
 
 
