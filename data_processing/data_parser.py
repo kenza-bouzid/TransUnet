52602af8de339e4999512c53ebdf1d4f759ea0bd
Kenzananas <bouzidkenza1@gmail.com>
2021-05-07 19:12:44 +0200

    clean data_parser

--- data_processing/data_parser.py
+++ data_processing/data_parser.py
@@ -3,15 +3,14 @@ from os.path import isfile, join
 from os import listdir
 from tqdm import tqdm
 
-import tensorflow_datasets as tfds
 import tensorflow as tf
 import numpy as np
 import cv2
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-
 BATCH_SIZE = 24
-N_CLASSES= 9
+N_CLASSES = 9
 BUFFER_SIZE = 50
+
 
 class DataWriter():
     def __init__(self, src_path, dest_path, batch_size=25, height=512, width=512):
@@ -81,7 +80,9 @@ class DataWriter():
         for i in tqdm(range(n_batches+1)):
             filename = self.dest_path + f'record_{i}.tfrecords'
             writer = tf.io.TFRecordWriter(filename)
-            start, end = self.batch_size*i, self.batch_size*(i+1) if self.batch_size*(i+1) < self.n_samples else self.n_samples
+            start, end = self.batch_size*i, self.batch_size * \
+                (i+1) if self.batch_size * \
+                (i+1) < self.n_samples else self.n_samples
             for file in self.filenames[start: end]:
                 data = np.load(self.src_path + file)
                 image, label = self.process_data(data)
@@ -95,45 +96,39 @@ class DataReader():
 
     def __init__(self, src_path="", height=512, width=512, depth=3):
         self.src_path = src_path
-
         self.filenames = [self.src_path + f for f in listdir(
             src_path) if isfile(join(src_path, f))]
-        
         self.height = height
         self.width = width
         self.depth = depth
 
     def parse_tfr_element(self, element):
         data = {
-            'label':tf.io.FixedLenFeature([], tf.string),
-            'image' : tf.io.FixedLenFeature([], tf.string),
-            }
+            'label': tf.io.FixedLenFeature([], tf.string),
+            'image': tf.io.FixedLenFeature([], tf.string),
+        }
 
-            
         content = tf.io.parse_single_example(element, data)
         raw_label = content['label']
         raw_image = content['image']
-        
-        
+
         image = tf.io.parse_tensor(raw_image, out_type=tf.float32)
-        image = tf.reshape(image, shape=[self.height,self.width,self.depth])
+        image = tf.reshape(image, shape=[self.height, self.width, self.depth])
 
         label = tf.io.parse_tensor(raw_label, out_type=tf.float32)
-        label = tf.reshape(label, shape=[self.height,self.width])
+        label = tf.reshape(label, shape=[self.height, self.width])
         label = tf.cast(label, tf.int32)
         label = tf.one_hot(label, depth=N_CLASSES)
         return (image, label)
 
     def get_dataset_small(self, filenames=None):
-        #create the dataset
+        # create the dataset
         filenames = self.filenames if filenames is None else filenames
         dataset = tf.data.TFRecordDataset(filenames)
-
-        #pass every single feature through our mapping function
+        # pass every single feature through our mapping function
         dataset = dataset.map(
             self.parse_tfr_element
         )
-            
         return dataset
 
     def load_dataset(self, filenames=None):
@@ -142,13 +137,14 @@ class DataReader():
         ignore_order.experimental_deterministic = False  # disable order, increase speed
         dataset = tf.data.TFRecordDataset(
             filenames, num_parallel_reads=AUTOTUNE)  # automatically interleaves reads from multiple files
-        dataset = dataset.with_options(ignore_order)  # uses data as soon as it streams in, rather than in its original order
+        # uses data as soon as it streams in, rather than in its original order
+        dataset = dataset.with_options(ignore_order)
         dataset = dataset.map(
             self.parse_tfr_element, num_parallel_calls=AUTOTUNE
         )
         # returns a dataset of (image, label) pairs if labeled=True or just images if labeled=False
         return dataset
-    
+
     def get_dataset(self, filenames=None):
         filenames = self.filenames if filenames is None else filenames
         dataset = self.load_dataset(filenames)
@@ -157,18 +153,17 @@ class DataReader():
         dataset = dataset.batch(BATCH_SIZE)
         return dataset
 
-
-    def load_dataset(self, filenames):
+    def load_dataset_tpu(self, filenames):
       # Read from TFRecords. For optimal performance, we interleave reads from multiple files.
         records = tf.data.TFRecordDataset(
             filenames, num_parallel_reads=AUTOTUNE)
         return records.map(self.parse_tfr_element, num_parallel_calls=AUTOTUNE)
 
     def get_training_dataset(self, train_fns, batch_size):
-        dataset = self.load_dataset(train_fns)
+        dataset = self.load_dataset_tpu(train_fns)
 
         # Create some additional training images by randomly flipping and
-        # increasing/decreasing the saturation of images in the training set. 
+        # increasing/decreasing the saturation of images in the training set.
         # def data_augment(image, one_hot_class):
         #     modified = tf.image.random_flip_left_right(image)
         #     modified = tf.image.random_saturation(modified, 0, 2)
@@ -178,7 +173,6 @@ class DataReader():
 
         # Prefetch the next batch while training (autotune prefetch buffer size).
         return dataset.shuffle(BUFFER_SIZE).batch(batch_size, drop_remainder=True).prefetch(AUTOTUNE)
-
 
     def get_dataset_tpu_training(self, tpu_strategy):
 
