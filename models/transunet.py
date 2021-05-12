b39a38da62ea812742a376842c64e79107568b08
miszkur <miszkurka.aga@gmail.com>
2021-05-12 19:50:02 +0200

    Fix dice loss

--- models/transunet.py
+++ models/transunet.py
@@ -115,7 +115,7 @@ class TransUnet():
         return 0.5 * cross_entropy_loss + 0.5 * dice_loss
 
     @tf.function
-    def gen_dice(y_true, y_pred, eps=1e-6):
+    def gen_dice(y_true, y_pred, eps=1e-5):
         """both tensors are [b, h, w, classes] and y_pred is in logit form"""
 
         # [b, h, w, classes]
@@ -128,23 +128,11 @@ class TransUnet():
         y_pred = tf.reshape(
             pred_tensor, [-1, y_true_shape[1]*y_true_shape[2], y_true_shape[3]])
 
-        # [b, classes]
-        # count how many of each class are present in
-        # each image, if there are zero, then assign
-        # them a fixed weight of eps
-        counts = tf.reduce_sum(y_true, axis=1)
-        weights = 1. / (counts ** 2)
-        weights = tf.where(tf.math.is_finite(weights), weights, eps)
-
-        multed = tf.reduce_sum(y_true * y_pred, axis=1)
-        summed = tf.reduce_sum(y_true + y_pred, axis=1)
-
-        # [b]
-        numerators = tf.reduce_sum(weights*multed, axis=-1)
-        denom = tf.reduce_sum(weights*summed, axis=-1)
-        dices = 1. - 2. * numerators / denom
-        dices = tf.where(tf.math.is_finite(dices), dices, tf.zeros_like(dices))
-        return tf.reduce_mean(dices)
+        intersect = tf.reduce_sum(y_true * y_pred)
+        y_sum = tf.reduce_sum(y_true * y_true)
+        z_sum = tf.reduce_sum(y_pred * y_pred)
+        loss = 1 - (2 * intersect + eps) / (z_sum + y_sum + eps)
+        return loss
 
 
     def resnet_embeddings(self, x):
