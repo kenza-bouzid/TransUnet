082221651a82ede0da5186e735a699c9f924f73b
miszkur <miszkurka.aga@gmail.com>
2021-05-20 11:31:09 +0200

    visualise single image

--- utils/visualize.py
+++ utils/visualize.py
@@ -17,3 +17,20 @@ def visualize(X, y, y_pred, sample_num, figsize=(10,10), cmap='viridis'):
     axis[1].set_title("predicted labels")
     plt.show()
 
+def visualize_non_empty_image(X, y, y_pred, figsize=(10,10), cmap='viridis'):
+    y_class = np.argmax(y_pred, axis=-1)
+
+    x = X.numpy()
+    y_np = y.numpy()
+    y_np = np.argmax(y_np, axis=-1)
+    labels = np.unique(y_np)
+    if len(labels) != 1:
+        fig, axis = plt.subplots(1, 2, figsize=figsize)
+        axis[0].imshow(x, cmap='gray') 
+        axis[0].imshow(y_np, cmap=cmap, alpha=0.3) 
+        axis[0].set_title("original labels")
+        axis[1].imshow(x, cmap='gray') 
+        axis[1].imshow(y_class[0], cmap=cmap, alpha=0.3)
+        axis[1].set_title("predicted labels")
+        plt.show()
+
