a945905d1fd5cda27b74707b7aaaf380f4844a6e
miszkur <miszkurka.aga@gmail.com>
2021-05-20 15:44:54 +0200

    change colormap

--- utils/visualize.py
+++ utils/visualize.py
@@ -29,6 +29,7 @@ def visualize_non_empty_predictions(X, y, models, figsize=(10, 10), cmap=pl.cm.D
     if len(labels) != 1:
         # create cmap
         my_cmap = cmap(np.arange(cmap.N))
+        my_cmap[:, -1] = 0.5
         my_cmap[0, -1] = 0.1
         my_cmap = ListedColormap(my_cmap)
 
@@ -36,7 +37,7 @@ def visualize_non_empty_predictions(X, y, models, figsize=(10, 10), cmap=pl.cm.D
         fig, axis = plt.subplots(1, n_plots, figsize=figsize)
 
         axis[0].imshow(x, cmap='gray')
-        axis[0].imshow(y_np, cmap=cmap, alpha=0.5)
+        axis[0].imshow(y_np, cmap=cmap)
         axis[0].set_title("original labels")
         axis[0].set_xticks([])
         axis[0].set_yticks([])
@@ -45,7 +46,7 @@ def visualize_non_empty_predictions(X, y, models, figsize=(10, 10), cmap=pl.cm.D
             y_pred = model.model.predict(tf.expand_dims(X, axis=0))
             y_class = np.argmax(y_pred, axis=-1)
             axis[i+1].imshow(x, cmap='gray')
-            axis[i+1].imshow(y_class[0], cmap=cmap, alpha=0.5)
+            axis[i+1].imshow(y_class[0], cmap=cmap)
             axis[i+1].set_title(f"{model.name}")
             axis[i+1].set_xticks([])
             axis[i+1].set_yticks([])
